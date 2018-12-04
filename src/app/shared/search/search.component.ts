b4dacea4cc34fa2a47730f5c5e052956e54275f6
whw58355 <whw58355@gmail.com>
2018-12-04 17:31:01 +0800

    finished search box func

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -10,7 +10,9 @@ import { Router } from '@angular/router'
 })
 export class SearchComponent implements OnInit {
     value = ''
-    message = 'this is alert message'
+    showProgressBar = false
+    showRequestError = false
+    showInvalidUrl = false
 
     constructor(
         private report: ReportProviderService,
@@ -21,19 +23,42 @@ export class SearchComponent implements OnInit {
     ngOnInit() {}
 
     handleClick() {
-        const dataUrl = this.url.getDataUrl(this.value)
-        if (!dataUrl) {
+        // do nothing when processing
+        if (this.showProgressBar) {
             return
         }
+
+        const dataUrl = this.url.getDataUrl(this.value)
+        if (!dataUrl) {
+            // show invalid url
+            this.showInvalidUrl = true
+            return
+        }
+
+        // show processing block
+        this.showProgressBar = true
+
         this.report.createReport(dataUrl).subscribe(
             async () => {
                 await this.router.navigateByUrl(`/report/${dataUrl}`)
                 console.log('success')
             },
             error => {
-                console.log('error')
+                console.log('error...')
                 console.log(error)
+                this.showRequestError = true
+                this.showProgressBar = false
+            },
+            () => {
+                console.log('complete ...')
+                // hide processing message block
+                this.showProgressBar = false
             }
         )
+    }
+
+    onValueChange(e) {
+        this.showRequestError = false
+        this.showInvalidUrl = false
     }
 }
